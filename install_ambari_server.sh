e3f08f2f655825f84d988b9119acfd296c1c2bf2
mragungsetiaji <mragungsetiaji@gmail.com>
2017-05-11 00:05:42 +0700

    Update install_ambari_server.sh

--- install_ambari_server.sh
+++ install_ambari_server.sh
@@ -3,7 +3,7 @@
 setenforce 0
 
 # Download Ambari Repository
-wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.2.0.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
+wget http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.5.0.3/ambari.repo
 
 # Install java-1.8
 yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
