96f1403c17a205b71a3d5c923f1428f6e2678fc7
mragungsetiaji <mragungsetiaji@gmail.com>
2017-05-12 11:11:59 +0700

    Update install_ambari_agent.sh

--- install_ambari_agent.sh
+++ install_ambari_agent.sh
@@ -14,12 +14,12 @@ yum -y install ambari-agent
 # set /etc/hosts
 echo '127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4' > /etc/hosts
 echo '::1 localhost localhost.localdomain localhost6 localhost6.localdomain6' >> /etc/hosts
-echo '192.168.111.2 node1.example.com' >> /etc/hosts
-echo '192.168.111.3 node2.example.com' >> /etc/hosts
-echo '192.168.111.4 node3.example.com' >> /etc/hosts
+echo '192.168.31.147 n001-hdp.kadence.local' >> /etc/hosts
+echo '192.168.31.148 n002-hdp.kadence.local' >> /etc/hosts
+echo '192.168.31.149 n003-hdp.kadence.local' >> /etc/hosts
 
 # replace localhost in node1
-cat /etc/ambari-agent/conf/ambari-agent.ini |sed 's/localhost/node1.example.com/g' > /etc/ambari-agent/conf/ambari-agent.ini.new
+cat /etc/ambari-agent/conf/ambari-agent.ini |sed 's/localhost/n001-hdp.kadence.local/g' > /etc/ambari-agent/conf/ambari-agent.ini.new
 mv -f /etc/ambari-agent/conf/ambari-agent.ini.new /etc/ambari-agent/conf/ambari-agent.ini
 
 # start ambari-agent
