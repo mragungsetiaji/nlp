a0a2c899347e3402d2856451850729308a748a79
Agung Setiaji <mragungsetiaji@gmail.com>
2018-11-12 08:46:07 +0700

    Add forward method

--- text_classification/models/lstm.py
+++ text_classification/models/lstm.py
@@ -29,3 +29,32 @@ class LSTMClassifier(nn.Module):
 		self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False) # Assigning the look-up table to the pre-trained GloVe word embedding.
 		self.lstm = nn.LSTM(embedding_length, hidden_size)
 		self.label = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, input_sentence, batch_size=None):
+	
+		""" 
+		Parameters
+		----------
+		input_sentence: input_sentence of shape = (batch_size, num_sequences)
+		batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
+		
+		Returns
+		-------
+		Output of the linear layer containing logits for positive & negative class which receives its input as the final_hidden_state of the LSTM
+		final_output.shape = (batch_size, output_size)
+		
+		"""
+		
+		''' Here we will map all the indexes present in the input sequence to the corresponding word vector using our pre-trained word_embedddins.'''
+		input = self.word_embeddings(input_sentence) # embedded input of shape = (batch_size, num_sequences,  embedding_length)
+		input = input.permute(1, 0, 2) # input.size() = (num_sequences, batch_size, embedding_length)
+		if batch_size is None:
+			h_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size).cuda()) # Initial hidden state of the LSTM
+			c_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size).cuda()) # Initial cell state of the LSTM
+		else:
+			h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
+			c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
+		output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
+		final_output = self.label(final_hidden_state[-1]) # final_hidden_state.size() = (1, batch_size, hidden_size) & final_output.size() = (batch_size, output_size)
+		
+		return final_output
