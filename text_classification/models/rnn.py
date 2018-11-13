b5d9fd92fab08d481985b06e9b3b314de5fe2054
Agung Setiaji <mragungsetiaji@gmail.com>
2018-11-13 08:03:18 +0700

    Update text_classification/models/rnn.py

--- text_classification/models/rnn.py
+++ text_classification/models/rnn.py
@@ -30,3 +30,32 @@ class RNN(nn.Module):
 		self.rnn = nn.RNN(embedding_length, hidden_size, num_layers=2, bidirectional=True)
 		self.label = nn.Linear(4*hidden_size, output_size)
 
+    def forward(self, input_sentences, batch_size=None):
+		
+		""" 
+		Parameters
+		----------
+		input_sentence: input_sentence of shape = (batch_size, num_sequences)
+		batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
+		
+		Returns
+		-------
+		Output of the linear layer containing logits for pos & neg class which receives its input as the final_hidden_state of RNN.
+		logits.size() = (batch_size, output_size)
+		
+		"""
+
+		input = self.word_embeddings(input_sentences)
+		input = input.permute(1, 0, 2)
+		if batch_size is None:
+			h_0 = Variable(torch.zeros(4, self.batch_size, self.hidden_size).cuda()) # 4 = num_layers*num_directions
+		else:
+			h_0 =  Variable(torch.zeros(4, batch_size, self.hidden_size).cuda())
+		output, h_n = self.rnn(input, h_0)
+		# h_n.size() = (4, batch_size, hidden_size)
+		h_n = h_n.permute(1, 0, 2) # h_n.size() = (batch_size, 4, hidden_size)
+		h_n = h_n.contiguous().view(h_n.size()[0], h_n.size()[1]*h_n.size()[2])
+		# h_n.size() = (batch_size, 4*hidden_size)
+		logits = self.label(h_n) # logits.size() = (batch_size, output_size)
+		
+		return logits
