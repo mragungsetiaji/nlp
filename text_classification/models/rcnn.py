a4b06a9f2a7907e1429174f0f1671cd5d8f0272e
Agung Setiaji <mragungsetiaji@gmail.com>
2018-11-15 08:25:23 +0700

    Update text_classification/models/rcnn.py

--- text_classification/models/rcnn.py
+++ text_classification/models/rcnn.py
@@ -39,3 +39,47 @@ class RCNN(nn.Module):
 		self.lstm                   = nn.LSTM(embedding_length, hidden_size, dropout=self.dropout, bidirectional=True)
 		self.W2                     = nn.Linear(2*hidden_size+embedding_length, hidden_size)
 		self.label                  = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input_sentence, batch_size=None):
+	
+		""" 
+		Parameters
+		----------
+		input_sentence: input_sentence of shape = (batch_size, num_sequences)
+		batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
+		
+		Returns
+		-------
+		Output of the linear layer containing logits for positive & negative class which receives its input as the final_hidden_state of the LSTM
+		final_output.shape = (batch_size, output_size)
+		
+		"""
+		
+		"""
+		
+		The idea of the paper "Recurrent Convolutional Neural Networks for Text Classification" is that we pass the embedding vector
+		of the text sequences through a bidirectional LSTM and then for each sequence, our final embedding vector is the concatenation of 
+		its own GloVe embedding and the left and right contextual embedding which in bidirectional LSTM is same as the corresponding hidden
+		state. This final embedding is passed through a linear layer which maps this long concatenated encoding vector back to the hidden_size
+		vector. After this step, we use a max pooling layer across all sequences of texts. This converts any varying length text into a fixed
+		dimension tensor of size (batch_size, hidden_size) and finally we map this to the output layer.
+		"""
+		input = self.word_embeddings(input_sentence) # embedded input of shape = (batch_size, num_sequences, embedding_length)
+		input = input.permute(1, 0, 2) # input.size() = (num_sequences, batch_size, embedding_length)
+		if batch_size is None:
+			h_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial hidden state of the LSTM
+			c_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial cell state of the LSTM
+		else:
+			h_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
+			c_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
+
+		output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
+		
+		final_encoding = torch.cat((output, input), 2).permute(1, 0, 2)
+		y = self.W2(final_encoding) # y.size() = (batch_size, num_sequences, hidden_size)
+		y = y.permute(0, 2, 1) # y.size() = (batch_size, hidden_size, num_sequences)
+		y = F.max_pool1d(y, y.size()[2]) # y.size() = (batch_size, hidden_size, 1)
+		y = y.squeeze(2)
+		logits = self.label(y)
+		
+		return logits
